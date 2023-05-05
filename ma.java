ce8f07a47d00a343f094965a94ab3892590b94ec
SL6I <s443004498@st.uqu.edu.sa>
2023-05-05 17:00:28 +0300

    STATIC AND VOID

---
+++ ma.java
@@ -0,0 +1,15 @@
+/**
+ * ma
+ */
+public class ma {
+static int a = 10;
+    static void p(){
+        a = 20;
+
+        System.out.println("Hi");
+    }
+    public static void main(String[] args) {
+        ma a = new ma();
+        System.out.println(ma.p());
+    }
+}
